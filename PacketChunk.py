126e4ca492311b3ff1c9d5cb393630d983497914
Omer Dan <aviram804@gmail.com>
2020-05-09 12:32:42 +0300

    bugFix, can now run on wireshark and find intrusions

--- PacketChunk.py
+++ PacketChunk.py
@@ -3,6 +3,13 @@
 class PacketChunk:
 
     def __init__(self, sender, receiver, amount, time):
+        """
+
+        :param sender:
+        :param receiver:
+        :param amount:
+        :param time:
+        """
         self.sender = sender
         self.receiver = receiver
         self.amount = amount
